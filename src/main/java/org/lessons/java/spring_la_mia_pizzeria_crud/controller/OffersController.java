ced17d2fb9eb5bc4c26af59d9664f2e1435db38d
Alessandro-Fiora <fiora.ale97@gmail.com>
2025-03-27 10:53:44 +0100

    feat: add offer edit

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/OffersController.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/OffersController.java
@@ -4,6 +4,7 @@ import org.lessons.java.spring_la_mia_pizzeria_crud.model.Offer;
 import org.lessons.java.spring_la_mia_pizzeria_crud.model.Pizza;
 import org.lessons.java.spring_la_mia_pizzeria_crud.repo.OfferRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -12,8 +13,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @Controller
@@ -32,8 +37,28 @@ public class OffersController {
 
         repo.save(formOffer);
 
-        return "redirect:/pizzas";
+        return "redirect:/pizzas/" + formOffer.getPizza().getId();
     }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model) {
+        model.addAttribute("edit", true);
+        model.addAttribute("offer", repo.findById(id).get());
+
+        return "offers/create-or-edit";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String update(@Valid @ModelAttribute("offer") Offer formOffer, BindingResult bindingResult, Model model ) {
+        if(bindingResult.hasErrors()){
+            return "offers/create-or-edit";
+        }
+        repo.save(formOffer);
+        
+        return "redirect:/pizzas/" + formOffer.getPizza().getId();
+    }
+    
+    
     
     
 }
